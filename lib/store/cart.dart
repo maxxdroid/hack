9d7f13cb90bcc311d76277953fc7bea771a7db60
maxxdroid <theonlymacmc@gmail.com>
2023-11-07 17:46:59 +0100

    first commit

--- lib/store/cart.dart
+++ lib/store/cart.dart
@@ -10,11 +10,68 @@ class Cart extends StatefulWidget {
 class _CartState extends State<Cart> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Text("SEARCH"),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          title: const Text("My Cart"),
+        ),
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
+              children: [
+                ListView(
+                  shrinkWrap: true,
+                  children: [
+                    Card(
+                      color: Colors.white,
+                      child: Container(
+                          height: 100,
+                          padding: const EdgeInsets.all(8),
+                          child: Row(
+                            children: [
+                              ClipRRect(
+                                  borderRadius: BorderRadius.circular(10),
+                                  child: Image.network(
+                                    "https://futonland.com/common/images/products/large/JOIN_Truffle_8.jpg",
+                                    width: 100,
+                                    fit: BoxFit.fitWidth,
+                                  )),
+                              const SizedBox(
+                                width: 5,
+                              ),
+                              const Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  SizedBox(
+                                    width: 190,
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text("Stylist soft chair"),
+                                        Icon(
+                                          Icons.favorite_outline_sharp,
+                                          color: Colors.black,
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  Text("Medium and soft", style: TextStyle(fontSize: 12),) ,
+                                  SizedBox(height: 20,),
+                                  Text("₵ 250", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),) 
+                                ],
+                              )
+                            ],
+                          )),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
